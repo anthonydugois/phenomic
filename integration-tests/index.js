01495f5f8042e5914e046469c363497bd82c8d70
Khoa Nguyen <hi@khoanguyen.me>
2016-09-15 02:00:12 +0700

    Fix integration-tests npm command

--- integration-tests/index.js
+++ integration-tests/index.js
@@ -1,5 +1,5 @@
 // This weird trick was used to make sure jest will run
 // build result test before cli test since cli will clean up dist folder
 
-require("../build-result.js")
-require("../cli.js")
+require("./build-result.js")
+require("./cli.js")
